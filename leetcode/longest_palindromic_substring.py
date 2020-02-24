0ded723638a6951ddad4fb9723cd0751b9cfa236
Ben Cook <808@holbertonschool.com>
2020-02-24 00:37:47 -0800

    finish early if solution has already been found

--- leetcode/longest_palindromic_substring.py
+++ leetcode/longest_palindromic_substring.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-from sys import argv
 
 
-def longest_palindrome(s):
-    longest = ''
-    l = len(s)
-    for ix in range(l):
-        i = j = 0
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = ''
+        l = len(s)
+        for ix in range(l):
+            if 2 * (l - ix) < len(longest):
+                break
+            i = j = 0
 
-        while ix - i >= 0 and ix + i < l and s[ix - i] == s[ix + i]:
-            i += 1
-        while ix - j >= 0 and ix + 1 + j < l and s[ix - j] == s[ix + 1 + j]:
-            j += 1
+            while ix >= i and ix + i < l and s[ix - i] == s[ix + i]:
+                i += 1
 
-        if 2 * i - 1 >= 2 * j:
-            cur = s[ix - i + 1 : ix + i]
-        else:
-            cur = s[ix - j + 1 : ix + 1 + j]
+            while ix >= j and ix + 1 + j < l and s[ix - j] == s[ix + 1 + j]:
+                j += 1
 
-        if len(cur) > len(longest):
-            longest = cur
+            if len(longest) < 2 * i - 1 >= 2 * j:
+                longest = s[ix - i + 1: ix + i]
+                ix += i
+            elif len(longest) < 2 * j:
+                longest = s[ix - j + 1: ix + 1 + j]
+                ix += j
 
-    return longest
-
-
-if __name__ == '__main__':
-    print(longest_palindrome(argv[1]))
-    print(longest_palindrome('banana'))
-    print(longest_palindrome('bananarama'))
-    print(longest_palindrome('abc'))
-    print(longest_palindrome('million'))
+        return longest
